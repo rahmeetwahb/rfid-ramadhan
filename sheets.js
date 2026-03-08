798eddc8a35edfc95320354554ebdeb251e24469
rahmeetwahb <rahmeet.abdulwahab@gmail.com>
2026-03-08 15:49:10 +0700

    edit nama sheet

--- sheets.js
+++ sheets.js
@@ -22,7 +22,7 @@ const SPREADSHEET_ID = process.env.SPREADSHEET_ID
 export async function getParticipant(uid) {
     const res = await sheets.spreadsheets.values.get({
         spreadsheetId: SPREADSHEET_ID,
-        range: "peserta-akhwat!A2:D100"
+        range: "peserta-ikhwan!A2:D100"
     })
 
     const rows = res.data.values
@@ -61,7 +61,7 @@ export async function saveAttendance(uid, name, session) {
 export async function alreadyScannedSession(uid, session) {
     const res = await sheets.spreadsheets.values.get({
         spreadsheetId: SPREADSHEET_ID,
-        range: "presensi-akhwat!A2:D1000"
+        range: "presensi-ikhwan!A2:D1000"
     })
 
     const rows = res.data.values || []
@@ -97,7 +97,7 @@ export async function getSessions() {
 export async function getLatestAttendance() {
     const res = await sheets.spreadsheets.values.get({
         spreadsheetId: SPREADSHEET_ID,
-        range: "presensi-akhwat!A2:D1000"
+        range: "presensi-ikhwan!A2:D1000"
     })
 
     const rows = res.data.values || []
